a95c0dc87b46edf9503d307f152fe271403db110
rahul-tech-03 <mrrahulpatil03@gmail.com>
2026-06-28 19:02:31 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AI_health_assistant",
+    version="0.1.0",
+    author="Rahul Chhagan Patil",
+    author_email="rp0205256@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
