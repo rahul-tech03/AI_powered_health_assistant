e35b7e9658eb53423dfb7290c1af9914cc2f36b6
rahul-tech-03 <mrrahulpatil03@gmail.com>
2026-06-28 18:55:59 +0530

    Huggungface Embedding model setup and otimized

--- src/helper.py
+++ src/helper.py
@@ -47,4 +47,3 @@ def download_hugging_face_embeddings():
     embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
     return embeddings
 
-
